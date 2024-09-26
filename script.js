89e2b552bcb59022320ebe7f711dc6af1964982d
D.Chirukesh <94205908+Chirukeshyt@users.noreply.github.com>
2024-09-26 14:31:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let hr = document.getElementById('hour');
 let min = document.getElementById('min');
 let sec = document.getElementById('sec');
 
-function displayTime(){
+function displayTime() {
     let date = new Date();
 
     // Getting hour, mins, secs from date
@@ -10,9 +10,9 @@ function displayTime(){
     let mm = date.getMinutes();
     let ss = date.getSeconds();
 
-    let hRotation = 30*hh + mm/2;
-    let mRotation = 6*mm;
-    let sRotation = 6*ss;
+    let hRotation = 30 * hh + mm / 2;
+    let mRotation = 6 * mm;
+    let sRotation = 6 * ss;
 
     hr.style.transform = `rotate(${hRotation}deg)`;
     min.style.transform = `rotate(${mRotation}deg)`;
